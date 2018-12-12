110bdd867aeac825a6fbcd76d1ba0351004f0d5c
Mauricio Silva <junior14_santos@hotmail.com>
2018-12-12 15:21:16 -0200

    Aula 21

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Simples from './components/Simples';
+import ParImpar from './components/Parimpar';
 
 export default class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
 				<Simples texto='Flex!' />
+				<ParImpar numero={34} />
 			</View>
 		);
 	};
