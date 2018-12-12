5c01fdc01b04e2f23bc1f6881c1a7e054f8cce73
Mauricio Silva <junior14_santos@hotmail.com>
2018-12-12 15:04:04 -0200

    Aula 20 - Projeto

--- exercicios/src/components/Simples.js
+++ exercicios/src/components/Simples.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
+import Padrao from './../styles/Padrao';
 
 /*export default function(props) {
     return <Text>{props.texto}</Text>
 };*/
 
-export default props => <Text>Arrow: {props.texto}</Text>;
+export default props => <Text style={Padrao.ex}>Arrow: {props.texto}</Text>;
