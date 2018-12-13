1ac254dbc7e7a6b82fb1dbdc2f33316124e07876
Mauricio Silva <junior14_santos@hotmail.com>
2018-12-13 19:32:56 -0200

    Aula 29

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -6,8 +6,12 @@ import { Inverter, MegaSena } from './components/Multi';
 import Contador from './components/Contador';
 import ValidarProps from './components/ValidarProps';
 import Evento from './components/Evento';
+import { Avo } from './components/ComunicacaoDireta';
 
 export default createDrawerNavigator({
+    Avo:{
+        screen: () => <Avo nome='Joao' sobrenome='Silva' />
+    },
     Evento: {
         screen: () => <Evento />
     },
