f0e5a91bd7ad5ece0ef331916037a9c2eec0b1c4
Mauricio Silva <junior14_santos@hotmail.com>
2018-12-28 01:22:40 -0200

    App Jogo Campo Minado

---
+++ mines/src/components/Header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, StyleSheet, Text,TouchableOpacity } from 'react-native';
+import Flag from './Flag';
+
+export default props => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.flagContainer}>
+                <TouchableOpacity onPress={props.onFlagPress}
+                style={styles.flagButton}>
+                    <Text>Minas</Text>
+                </TouchableOpacity>
+                <Text style={styles.flagsLeft}>= {props.flagsLeft}</Text>
+            </View>
+            <TouchableOpacity onPress={props.onNewGame}
+                style={styles.button}>
+                    <Text style={styles.buttonLabel}>Novo Jogo</Text> 
+                </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({ 
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#EEE',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        paddingTop: 20,
+        paddingHorizontal: 20,
+    },
+    flagContainer: {
+        marginTop: 10,
+        minWidth: 30,
+    },
+    flagsLeft: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        paddingTop: 5,
+        marginLeft: 20,
+    },
+    button: {
+        backgroundColor:'#999',
+        padding: 5,
+    },
+    buttonLabel: {
+        fontSize: 20,
+        color:'#DDD',
+        fontWeight: 'bold'
+    }
+});
